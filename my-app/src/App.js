b789d2da3a6586942418116adf8919f8db194dc6
sjambon <steven.jambon1@gmail.com>
2021-04-09 16:33:26 +0200

    💄Added TailwindCSS basic config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import MemberRouter from "./routers/MemberRouter";
 import RegisterPage from "./pages/authentication/RegisterPage";
