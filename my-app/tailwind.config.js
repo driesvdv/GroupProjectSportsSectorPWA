43439e3f38f1d031c4b1b5d5f97725c9e80844f7
Arthur Deblaere <arthur.deblaere@hotmail.be>
2021-04-27 19:02:36 +0200

    add design colors

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,6 +2,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      blue: {
+        DEFAULT: '#114B5F',
+        dark: '#25282B',
+      },
+      green: {
+        DEFAULT: '#1A936F',
+        dark: '#009eeb',
+      },
+      gray: {
+        DEFAULT: '#C4C4C4',
+        dark: '#7B7B7B'
+      },
+      red: {
+        DEFAULT: '#CF6766'
+      }
+    },
     extend: {},
   },
   variants: {
