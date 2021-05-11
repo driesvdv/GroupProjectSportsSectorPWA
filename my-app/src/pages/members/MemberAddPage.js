8b24c7c87b6659ea2efce21e47331bb5fcce73f2
Arthur Deblaere <arthur.deblaere@hotmail.be>
2021-05-11 18:46:10 +0200

    skeleton member add page

--- my-app/src/pages/members/MemberAddPage.js
+++ my-app/src/pages/members/MemberAddPage.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
 function MemberAddPage(props) {
+    const handleSubmit = (e) => {
+        alert('submitted')
+        e.preventDefault();
+    }
+
     return (
-        <div>add member</div>
-    );
+        <div>
+            <div>
+                <p>Leden</p>
+                <p>Toevoegen</p>
+            </div>
+            <form onSubmit={e => { handleSubmit(e) }}>
+
+                <input
+                    name='voornaam'
+                    type='text'
+                    placeholder='Voornaam'
+                />
+                <input
+                    name='achternaal'
+                    type='text'
+                    placeholder='Achternaam'
+                />
+                <input
+                    name='geboortedatum'
+                    type='text'
+                    placeholder='Geboortedatum'
+                />
+                <input
+                    type='submit'
+                    value='Toevoegen'
+                />
+            </form>
+        </div>
+    )
 }
 
 export default MemberAddPage;
