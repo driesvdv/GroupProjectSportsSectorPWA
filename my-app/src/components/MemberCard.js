a3bd7eb787f67cc73c302bddbd59bd8c88c3996c
Arthur Deblaere <arthur.deblaere@hotmail.be>
2021-05-16 14:26:09 +0200

    membercard

--- my-app/src/components/MemberCard.js
+++ my-app/src/components/MemberCard.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import moment from 'moment';
 
 function MemberCard({ member }) {
     return (
-        <a className={"border-gray p-2 mx-auto shadow flex my-5 rounded hover:shadow-dark"}
-            href={`/members/${member.id}`}>
+        <NavLink to={`/leden/${member.id}`} className={"border-gray p-2 mx-auto shadow flex justify-between my-5 z-30 rounded hover:shadow-dark"}>
             <div className={"pb-2"}>
-                <p className={"font-montserrat font-medium text-lg"}>{member.firstName} {member.lastName}</p>
-                <p className={"text-base text-green font-normal"}>{moment.duration(moment().diff(member.birthDate)).asYears().toFixed(0)} jaar</p>
+                <p className={"font-montserrat font-medium text-lg"}>{member.first_name} {member.last_name}</p>
+                <p className={"text-base text-green font-normal"}>{moment.duration(moment().diff(member.birth_date)).asYears().toFixed(0)} jaar</p>
             </div>
-
-        </a>
+            <div className={"flex flex-col justify-center mr-4"}>
+                <img src={process.env.PUBLIC_URL + '/assets/chevron-right.svg'} />
+            </div>
+        </NavLink>
 
     );
 }
