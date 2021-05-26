023f21e4b54c2754362acab774ec217cfabe6880
Dries Van de Velde <73098090+driesvdv@users.noreply.github.com>
2021-05-26 18:30:42 +0200

    changed api url

--- my-app/src/services/authentication.service.js
+++ my-app/src/services/authentication.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'http://api.sportplus.vandevelde.studio/api/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
